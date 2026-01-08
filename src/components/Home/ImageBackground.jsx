cafd26efa514b3f96afc2a8596d5443df30b0220
Omkar Jagtap <omkarjagtap8443@gmail.com>
2026-01-08 10:55:33 +0530

    search card

--- src/components/Home/ImageBackground.jsx
+++ src/components/Home/ImageBackground.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 // Go up two levels (../../) to reach the src folder, then into assets
 import BgBus from "../../assets/Images/BgBus.png"; 
+import Seacrh from "./Seacrh";
 
 const ImageBackground = () => {
   return (
     <div 
-      className="relative w-full h-[100dvh] overflow-hidden bg-center bg-cover bg-no-repeat"
+      className="  relative w-full h-screen overflow-hidden bg-center bg-cover bg-no-repeat"
       style={{ backgroundImage: `url(${BgBus})` }}
     >
       {/* Dark Overlay to make the text pop */}
-      <div className="absolute inset-0 bg-black/50"></div>
+
+
+
 
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-6">
+           <Seacrh/>
+    
         <h1 className="text-white/90 text-4xl md:text-6xl font-bold italic tracking-tight uppercase">
+        
+   
          Welcome To <br /> Raj <span className="text-orange-500">Mudra</span> Travels
         </h1>
         <p className="text-gray-200 mt-4 max-w-xl text-lg md:text-2xl font-light">
---
+++ src/components/Home/Seacrh.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Seacrh = () => {
+  return (
+ 
+      <div className="w-full max-w-6xl bg-white/40 h-20 rounded-md mx-auto my-2">
+        <div className="flex justify-between p-4">
+          <ul className="flex gap-2 ">
+            <li className="font-bold cursor-pointer hover:bg-orange-400 rounded-md px-2 hover:text-white  ">Buses</li>
+            <li className="font-bold cursor-pointer hover:bg-orange-400 rounded-md px-2 hover:text-white">Buses</li>
+            <li className="font-bold  cursor-pointer hover:bg-orange-400 rounded-md px-2 hover:text-white">Buses</li>
+            <li className="font-bold cursor-pointer hover:bg-orange-400 rounded-md px-2 hover:text-white">Buses</li>
+          </ul>
+
+         <h2 className='font-bold'>India's Fastest Bus Ticket Booking Platform</h2>
+        </div>
+       
+
+      </div>
+        
+  )
+}
+
+export default Seacrh
