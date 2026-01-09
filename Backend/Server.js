d0154827ef255911fd73e928bba07324889d0b65
Omkar Jagtap <omkarjagtap8443@gmail.com>
2026-01-09 17:39:30 +0530

    cors

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app=express();
 
@@ -7,10 +8,23 @@ dotenv.config();
 
 const PORT=process.env.PORT || 3000;
 
+app.use(cors({
+origin:"http://localhost:5173/",
+ credentials: true
+}))
+
+app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.send('Bus Booking System Backend is running');
 })
 
+app.get('/cors',(req,res)=>{
+    res.send('working okay cors')
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+
+    
 });
